bae328b10715882aff0e92639ba94ba9c5dbba1d
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-05 18:00:22 -0700

    most functionality is completed. next is polish

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,9 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBoard = StoryBoard()
+    
+    @IBAction func submitChoice(_ sender: UIButton) {
+        storyBoard.goToNext(selection: sender.currentTitle!)
+        updateUi()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUi()
+    }
+    
+    @objc func updateUi(){
+        let position = storyBoard.currDest
+        storyLabel.text = storyBoard.story[position].title
+        choice1Button.setTitle(storyBoard.story[position].choice1, for: .normal)
+        choice2Button.setTitle(storyBoard.story[position].choice2, for: .normal)
     }
 
 
